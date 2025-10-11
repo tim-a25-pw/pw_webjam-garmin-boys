dd6f374ba46336ed20d24bad52d24fe37f213538
Farashev Dzhulian <dzhulianfarashev@gmail.com>
2025-10-11 10:47:53 -0400

    header css, submenu ajustement

--- theme/footer.php
+++ theme/footer.php
@@ -1,6 +1,28 @@
     <footer class="footer">
         <div class="wrapper">
-            
+            <!--LOGO-->
+            <a href="">
+                <svg class="icon">
+                    <use href="#icon-LOGO"></use>
+                </svg>
+            </a>
+            <!--NAV MENU-->
+            <nav class="nav">
+                <ul class="nav__list">
+                    <li class="nav__item"><a href="" class="nav__link">LIEN</a></li>
+                    <li class="nav__item"><a href="" class="nav__link">LIEN</a></li>
+                    <li class="nav__item"><a href="" class="nav__link">LIEN</a></li>
+                </ul>
+            </nav>
+            <!--LIST SOCIALS-->
+            <ul class="socials">
+                <li><a href="">SOCIAL 1</a></li>
+                <li><a href="">SOCIAL 2</a></li>
+                <li><a href="">SOCIAL 3</a></li>
+                <li><a href="">SOCIAL 4</a></li>
+            </ul>
+            <!--COPYRIGHT-->
+            <p class="footer__text">© Garmin Boys | Webjam 2025 | Tous droits réservés</p>
         </div>
     </footer>
         <?php wp_footer(); ?>
